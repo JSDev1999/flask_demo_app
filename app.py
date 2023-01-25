fd4a9147c15724509185000d9fdf96bf3bc9981a
JSDev1999 <rameshlearningadvance@gmail.com>
2023-01-25 10:01:36 +0530

    updated

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/about')
+def hello_about():
+    return 'Hello World! from about page!!'
+
 if __name__ == '__main__':
     app.run()
