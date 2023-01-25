e2831f8760aad174c3015a52dbb75a2ee7176934
JSDev1999 <rameshlearningadvance@gmail.com>
2023-01-25 14:17:15 +0530

    updated

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return  render_template('index.html')
 
 @app.route('/about')
 def hello_about():
-    return 'Hello World! from about page!!'
+    return render_template('about.html')
+
+@app.route('/post')
+def hello_post():
+    return render_template('post.html')
+
+@app.route('/contact')
+def hello_contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
