e9091b8dc0ecea3ca1167e1abe74ffe5bf9e67fe
JSDev1999 <rameshlearningadvance@gmail.com>
2023-02-06 12:22:29 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template, url_for
+import requests
+
+url = "https://bing-news-search1.p.rapidapi.com/news"
+
+querystring = {"safeSearch":"Off","textFormat":"Raw"}
+
+headers = {
+	"X-BingApis-SDK": "true",
+	"X-RapidAPI-Key": "63d28da5bemsh5cf54013327a228p1cb755jsn23338e042adb",
+	"X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com"
+}
+
+responsedata = requests.request("GET", url, headers=headers, params=querystring)
+finalData = responsedata.text
+print(finalData)
+name='ramesh'
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return  render_template('index.html')
+    return  render_template('index.html', posts=responsedata.text , name=name)
+    #return response.text
 
 @app.route('/about')
 def hello_about():
@@ -20,4 +37,4 @@ def hello_contact():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
